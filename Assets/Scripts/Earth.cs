3bf71a2ed448acd69e5e255294453d1319854fb1
Andrew Daly <andrewdalyslash@gmail.com>
2015-02-16 22:47:36 +0000

    first commit

---
+++ Assets/Scripts/Earth.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Earth : MonoBehaviour {
+
+
+	public float rotationsPerMinute = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,6.0f*rotationsPerMinute*Time.deltaTime,6.0f*rotationsPerMinute*Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PathFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollow : MonoBehaviour {
+
+
+	public GameObject Waypoint;
+	public float speed = 1.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, Waypoint.transform.position, speed);
+	}
+}
